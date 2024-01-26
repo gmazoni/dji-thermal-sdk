bcd845f7b55dddce36c9730f5910d6e1899d9c23
Guilherme Mazoni <gmazoni@gmail.com>
2024-01-26 19:07:01 -0300

    Output tiff with sharp

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const { version, getTemperatureData } = require('./');
 
+const sharp = require('sharp')
+const { decode } = require('tiff')
 const fs = require('fs')
 
 const imageBuffer = fs.readFileSync('test.jpg')
@@ -11,5 +13,17 @@ console.log(version())
 console.timeEnd('version')
 
 console.time('getTemperatureData')
-console.log(getTemperatureData(imageBuffer))
+const { width, height, parameters, data } = getTemperatureData(imageBuffer)
 console.timeEnd('getTemperatureData')
+
+const buffer = Buffer.from(data)
+// Create a Sharp image from the buffer
+const sharpImage = sharp(buffer, { raw: { width, height, channels: 1 } });
+
+sharpImage.toFile('test.tiff', (err, info) => {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('Image saved:', info);
+    }
+});
